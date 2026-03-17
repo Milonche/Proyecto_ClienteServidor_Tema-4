0e70e02c063749cdf14186b23157f4bd3533d865
dexor <dexor@DESKTOP-06SJ0VU>
2026-03-17 14:47:33 -0600

    Se crea clase Pedido con metodos para agregar y remover productos

---
+++ src/proyecto_clienteservidor_jeremyulate/Pedido.java
@@ -0,0 +1,31 @@
+package proyecto_clienteservidor_jeremyulate;
+import java.util.ArrayList;
+public class Pedido {
+    private ArrayList<Producto> productos;
+    private String estado;
+    
+    public Pedido(){
+        productos = new ArrayList<>();
+        estado = "Pendiente";
+    }
+    
+    public void agregarProductos(Producto p){
+        productos.add(p);
+    }
+    
+    public void quitarProducto(Producto p){
+        productos.remove(p);
+    }
+    
+    public double calcularTotal(){
+        double total = 0;
+        for(Producto p : productos){
+            total += p.calcularPrecio();
+        }
+        return total;
+    }
+    
+    public void getEstado(){
+        this.estado = estado;
+    }
+}
