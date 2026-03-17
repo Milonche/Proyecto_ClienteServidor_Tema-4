eb3cb85e4a8453eee179ac64813c890e8b714d40
dexor <dexor@DESKTOP-06SJ0VU>
2026-03-17 13:53:50 -0600

    Se agregan las clases principales: Producto y Usuario

--- src/proyecto_clienteservidor_jeremyulate/Main.java
+++ src/proyecto_clienteservidor_jeremyulate/Main.java
@@ -2,7 +2,7 @@ package proyecto_clienteservidor_jeremyulate;
 public class Main {
 
     public static void main(String[] args) {
-        /
+        //
     }
     
 }
---
+++ src/proyecto_clienteservidor_jeremyulate/Usuario.java
@@ -0,0 +1,18 @@
+package proyecto_clienteservidor_jeremyulate;
+public class Usuario {
+    private String nombre;
+    private String rol;
+    
+    public Usuario(String nombre, String rol){
+        this.nombre = nombre;
+        this.rol = rol;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public String getRol(){
+        return rol;
+    }
+}
